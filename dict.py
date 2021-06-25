f8d07517ce5969abe8d5bacd456efa8572921bb6
ycatblue <215370685@qq.com>
2021-06-25 19:01:08 +0800

    update intro.txt

--- dict.py
+++ dict.py
@@ -1,2 +1,15 @@
-d = {'aod': 89, 'andy': 100, 'candy': 76}
-d['aod']
+d = {"aod": 89, "andy": 100, "candy": 76}
+x = d["aod"]
+y = d.keys()
+print(x)
+print(y)
+
+d["aod"] = 22
+d["anna"] = 100
+print(y)
+print(d)
+
+if "aod" in d:
+    print("yes,is in d")
+else:
+    print("no")
