3b531bb930dc7d5ced88ca9529021578c8da1857
ycatblue <215370685@qq.com>
2021-11-24 18:55:48 +0800

    update intro.txt

--- Python Number.py
+++ Python Number.py
@@ -1,3 +1,11 @@
 import random
 
 print(random.randrange(1, 10))
+
+# 输出保留一位小数输出、字符串、整数输出
+print('%.1f' % 123.32)
+print('%.s' % 'hello world')
+print('%d' % 20.32)
+
+
+
---
+++ task2-for.py
@@ -0,0 +1,7 @@
+# 使用for循环计算1到100的和。
+
+num = 0
+for i in range(101):
+    num += i
+
+print(num)
