11389bcad2f1b5c10038116d606cdfb1132fe4a6
ycatblue <215370685@qq.com>
2021-12-08 18:39:44 +0800

    update intro.txt

--- task8-comprehensions.py
+++ task8-comprehensions.py
@@ -3,6 +3,14 @@ a_str = 'python'
 print([i for i in a_str])
 
 # 求出所有小于20的偶数
-list = [i for i in range(20) if i % 2 == 0]
-print(list)
+a_list = [i for i in range(20) if i % 2 == 0]
+print(a_list)
 
+# 互换字典中的key和value
+a_dict = {'py': 'python', 'sel': 'selenium', 'at': 'autotest'}
+dict_conversion = {v: k for k, v in a_dict.items()}
+print(dict_conversion)
+
+# 求出小于10的所有数的平方。
+dict_k = {x ** 2 for x in range(1, 10)}
+print(dict_k)
