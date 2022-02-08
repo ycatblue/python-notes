6209433c9b36d20e7f048c2fb5b47769bedf1876
ycatblue <215370685@qq.com>
2022-02-08 18:59:32 +0800

    update intro.txt

--- selenium WebDriver-Object Manipulation.py
+++ selenium WebDriver-Object Manipulation.py
@@ -3,3 +3,21 @@ from selenium.webdriver.common.by import By
 import time
 
 driver = webdriver.Chrome()
+driver.get("http://localhost:8080/projectHtml/chapter1/period2/index.html")
+
+# 单击事件
+# driver.find_element(By.ID, "btn-login").click()
+
+# 在输入账号的输入框中输入内容“tynam@test.com”，然后清空输入的内容，以id='email'进行定位。
+driver.find_element(By.ID, "email").send_keys("tynam@test.com")
+time.sleep(1)
+driver.find_element(By.ID, "email").clear()
+
+# 获取文本内容
+login_text = driver.find_element(By.ID, "btn-login").text
+print(login_text)
+
+# 获取账号输入框的name值，使用id='email'进行定位。
+name_text = driver.find_element(By.ID, "email").get_attribute("name")
+print(name_text)
+
